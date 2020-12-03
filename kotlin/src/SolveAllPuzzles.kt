51621008ea4b99ad5d889cb1881dc2964a7a511f
Kristof <kristof.achten@uzleuven.be>
2020-12-03 13:04:49 +0100

    Solved D3 in Kotlin

--- kotlin/src/SolveAllPuzzles.kt
+++ kotlin/src/SolveAllPuzzles.kt
@@ -1,10 +1,11 @@
 import challenges.Puzzle
 import challenges.day1.D1ExpenseReport
 import challenges.day2.D2Passwords
+import challenges.day3.D3Slopes
 
 
 private val puzzles = mutableListOf<Puzzle>(
-    D1ExpenseReport(), D2Passwords()
+    D1ExpenseReport(), D2Passwords(), D3Slopes()
 )
 
 fun main() {
---
+++ kotlin/src/challenges/day3/D3Slopes.kt
@@ -0,0 +1,45 @@
+package challenges.day3
+
+import challenges.Puzzle
+
+class D3Slopes : Puzzle() {
+    val input = create2DArray(getInput(3).split('\n'))
+    val tree = "#"
+
+    private fun create2DArray(parts: List<String>): Array<Array<String>> {
+        return Array(parts.size) { i ->
+            Array(parts.get(i).length) { j ->
+                parts.get(i)[j].toString()
+            }
+        }
+    }
+
+    override fun part1(): String {
+        return determineTreesForSlope(3, 1).toString()
+    }
+
+    override fun part2(): String {
+        val multTrees = determineTreesForSlope(1, 1) *
+                determineTreesForSlope(3, 1) *
+                determineTreesForSlope(5, 1) *
+                determineTreesForSlope(7, 1) *
+                determineTreesForSlope(1, 2)
+        return multTrees.toString()
+    }
+
+    fun determineTreesForSlope(difX: Int, difY: Int) : Int {
+        var curX = 0
+        var curY = 0
+        var trees = 0
+
+        while (curY <= (input.size - difY)) {
+            if (input[curY][curX] == tree) {
+                trees++
+            }
+            curX = (curX + difX) % input[0].size
+            curY += difY
+        }
+
+        return trees
+    }
+}
