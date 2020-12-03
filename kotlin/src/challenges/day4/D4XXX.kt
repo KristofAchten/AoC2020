0c5ab92b66b336ac40bbc1cd4fc29911f3618323
Kristof <kristof.achten@uzleuven.be>
2020-12-04 00:21:51 +0100

    Setup day 4

---
+++ kotlin/src/challenges/day4/D4XXX.kt
@@ -0,0 +1,13 @@
+package challenges.day4
+
+import challenges.Puzzle
+
+class D4XXX : Puzzle(4) {
+    override fun part1(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): String {
+        TODO("Not yet implemented")
+    }
+}
