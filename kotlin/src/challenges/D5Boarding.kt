a1217ff094fac326143a4756568dd71fa0f7c08c
Kristof <kristof.achten@uzleuven.be>
2020-12-05 18:54:18 +0100

    Got tipped off with a nicer solution for day 5

--- kotlin/src/challenges/D5Boarding.kt
+++ kotlin/src/challenges/D5Boarding.kt
@@ -1,38 +1,21 @@
 package challenges
 
+import java.lang.IllegalStateException
+import java.lang.Integer.parseInt
 import kotlin.math.ceil
 
 class D5Boarding : Puzzle(5) {
     val input = rawInput.split("\n")
 
-    override fun part1(): String {
-        return input.map { determineSeatID(it) }.max().toString()
-    }
+    override fun part1() = input.map { determineSeatID(it) }.max().toString()
 
     override fun part2(): String {
-        var sortedRes = input.map { determineSeatID(it) }.toList().sorted()
-        var curVal = sortedRes.get(0)
-
-        for (i in 0 until sortedRes.size) {
-            if (sortedRes.get(i) != curVal++) {
-                break
-            }
-        }
-        return (curVal - 1).toString()
+        val res = input.map { determineSeatID(it) }.toList().sorted()
+        return (res.get(res.indices.find { i -> res[i] + 1 != res[i + 1] }!!) + 1).toString()
     }
 
     fun determineSeatID(seq: String): Int {
-        var row = Pair(0, 127)
-        var col = Pair(0, 7)
-
-        for (i in 0 until seq.length) {
-            when (seq[i]) {
-                'F' -> row = Pair(row.first, row.second - ceil((row.second - row.first) / 2.0).toInt())
-                'B' -> row = Pair(row.first + ceil((row.second - row.first) / 2.0).toInt(), row.second)
-                'L' -> col = Pair(col.first, col.second - ceil((col.second - col.first) / 2.0).toInt())
-                'R' -> col = Pair(col.first + ceil((col.second - col.first) / 2.0).toInt(), col.second)
-            }
-        }
-        return row.first * 8 + col.first
+        val binSeq = seq.replace("F", "0").replace("L", "0").replace("B", "1").replace("R", "1")
+        return parseInt(binSeq.substring(0, 7), 2) * 8 + parseInt(binSeq.substring(7), 2)
     }
 }
