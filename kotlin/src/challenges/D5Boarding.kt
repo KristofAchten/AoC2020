35b7d09545e7e01121e8c6b0c585e2a449d804de
Kristof <kristof.achten@uzleuven.be>
2020-12-05 19:41:17 +0100

    Removed some code duplication for day 5

--- kotlin/src/challenges/D5Boarding.kt
+++ kotlin/src/challenges/D5Boarding.kt
@@ -5,14 +5,10 @@ import java.lang.Integer.parseInt
 import kotlin.math.ceil
 
 class D5Boarding : Puzzle(5) {
-    val input = rawInput.split("\n")
+    val ids = rawInput.split("\n").map { determineSeatID(it) }.toList().sorted()
 
-    override fun part1() = input.map { determineSeatID(it) }.max().toString()
-
-    override fun part2(): String {
-        val res = input.map { determineSeatID(it) }.toList().sorted()
-        return (res.get(res.indices.find { i -> res[i] + 1 != res[i + 1] }!!) + 1).toString()
-    }
+    override fun part1() = ids.max().toString()
+    override fun part2() = (ids.get(ids.indices.find { i -> ids[i] + 1 != ids[i + 1] }!!) + 1).toString()
 
     fun determineSeatID(seq: String): Int {
         val binSeq = seq.replace("F", "0").replace("L", "0").replace("B", "1").replace("R", "1")
