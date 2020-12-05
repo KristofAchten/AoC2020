073432148cd5ed557aadd5db8d31f06e15c0d96d
Kristof <kristof.achten@uzleuven.be>
2020-12-05 21:28:58 +0100

    D5: now using toInt instead of parseInt

--- kotlin/src/challenges/D5Boarding.kt
+++ kotlin/src/challenges/D5Boarding.kt
@@ -7,7 +7,7 @@ import kotlin.math.ceil
 class D5Boarding : Puzzle(5) {
     val ids = rawInput.split("\n").map {
         val binSeq = it.replace("(F|L)".toRegex(), "0").replace("(B|R)".toRegex(), "1")
-        parseInt(binSeq.substring(0, 7), 2) * 8 + parseInt(binSeq.substring(7), 2)
+        binSeq.substring(0, 7).toInt(2) * 8 + binSeq.substring(7).toInt(2)
     }.toList().sorted()
 
     override fun part1() = ids[ids.size - 1].toString()
