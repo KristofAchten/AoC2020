d01296ecb2a08f3b7c3b90bd98b29fa34a1258c9
Kristof <kristof.achten@uzleuven.be>
2020-12-05 21:19:18 +0100

    D5: removed unnecessary function

--- kotlin/src/challenges/D5Boarding.kt
+++ kotlin/src/challenges/D5Boarding.kt
@@ -5,13 +5,11 @@ import java.lang.Integer.parseInt
 import kotlin.math.ceil
 
 class D5Boarding : Puzzle(5) {
-    val ids = rawInput.split("\n").map { determineSeatID(it) }.toList().sorted()
+    val ids = rawInput.split("\n").map {
+        val binSeq = it.replace("(F|L)".toRegex(), "0").replace("(B|R)".toRegex(), "1")
+        parseInt(binSeq.substring(0, 7), 2) * 8 + parseInt(binSeq.substring(7), 2)
+    }.toList().sorted()
 
     override fun part1() = ids.max().toString()
     override fun part2() = (ids.get(ids.indices.find { i -> ids[i] + 1 != ids[i + 1] }!!) + 1).toString()
-
-    fun determineSeatID(seq: String): Int {
-        val binSeq = seq.replace("(F|L)".toRegex(), "0").replace("(B|R)".toRegex(), "1")
-        return parseInt(binSeq.substring(0, 7), 2) * 8 + parseInt(binSeq.substring(7), 2)
-    }
 }
