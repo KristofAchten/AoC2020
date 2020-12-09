b50a8d024a87b8de24931076888c297f1c6e0557
Kristof <kristof.achten@uzleuven.be>
2020-12-09 08:44:21 +0100

    D9: full solution, to be optimized.

---
+++ kotlin/src/challenges/D9Encoding.kt
@@ -0,0 +1,40 @@
+package challenges
+
+class D9Encoding : Puzzle(9) {
+
+    val input = rawInput.split("\n").map { it.toLong() }
+    val invalidNumber = findInvalidNumber(25)
+
+
+    override fun part1() = invalidNumber.toString()
+    override fun part2() = findContiguosSetAndSumMinMax(invalidNumber).toString()
+
+    private fun findInvalidNumber(preambleSize: Int): Long {
+        for (i in preambleSize..input.size) {
+            if (!pairExists(input.get(i), input.subList(i - preambleSize, i))) return input.get(i)
+        }
+        throw IllegalStateException("Could not determine an invalid number!")
+    }
+
+    private fun pairExists(value: Long, subList: List<Long>) = subList.any {
+        input.contains(value - it)
+    }
+
+
+    private fun findContiguosSetAndSumMinMax(invalidNumber: Long): Long {
+        for (i in input.indices) {
+            var sum = 0L
+            var curIdx = i
+
+            while (curIdx < input.size && sum < invalidNumber) {
+                sum += input.get(curIdx++)
+            }
+
+            if (sum == invalidNumber) {
+                val subList = input.subList(i, curIdx - 1)
+                return subList.min()!! + subList.max()!!
+            }
+        }
+        throw IllegalStateException("Could not find contiguous set!")
+    }
+}
