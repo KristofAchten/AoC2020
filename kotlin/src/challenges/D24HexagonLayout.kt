b3ce9b51cd56f5e8be6689095256b72258901953
Kristof <kristof.achten@uzleuven.be>
2020-12-25 01:21:09 +0100

    D24: rename

--- kotlin/src/challenges/D24HexagonLayout.kt
+++ kotlin/src/challenges/D24HexagonLayout.kt
@@ -1,17 +1,17 @@
 package challenges
 
-data class Point2D(val x: Int, val y: Int) : Point {
+data class HexPoint2D(val x: Int, val y: Int) : Point {
     override fun getNeighbours(): List<Point> {
         val neighbours = mutableListOf<Point>()
         for (x in this.x - 1..this.x + 1)
             for (y in this.y - 1..this.y + 1)
-                neighbours.add(Point2D(x, y))
+                neighbours.add(HexPoint2D(x, y))
 
         neighbours.removeAll(
                 listOf(
-                        Point2D(this.x, this.y),
-                        Point2D(this.x - 1, this.y + 1),
-                        Point2D(this.x + 1, this.y - 1)
+                        HexPoint2D(this.x, this.y),
+                        HexPoint2D(this.x - 1, this.y + 1),
+                        HexPoint2D(this.x + 1, this.y - 1)
                 )
         )
 
@@ -26,7 +26,7 @@ class D24HexagonLayout : Puzzle(24) {
     override fun part1() = initialTiles.size.toString()
     override fun part2() = age(initialTiles, 100).size.toString()
 
-    private fun decorate(input: List<String>): Set<Point2D> {
+    private fun decorate(input: List<String>): Set<HexPoint2D> {
         val ops = input.map { line ->
             var x = 0;
             var y = 0;
@@ -40,10 +40,10 @@ class D24HexagonLayout : Puzzle(24) {
                     "ne", "nw" -> y--
                 }
             }
-            Point2D(x, y)
+            HexPoint2D(x, y)
         }
 
-        val result = hashSetOf<Point2D>()
+        val result = hashSetOf<HexPoint2D>()
         ops.forEach {
             if (result.contains(it)) {
                 result.remove(it)
@@ -55,14 +55,14 @@ class D24HexagonLayout : Puzzle(24) {
         return result
     }
 
-    private fun age(initialTiles: Set<Point2D>, turns: Int): Set<Point2D> {
+    private fun age(initialTiles: Set<HexPoint2D>, turns: Int): Set<HexPoint2D> {
         var curTiles = initialTiles.toMutableSet()
 
         repeat(turns) {
             curTiles = curTiles
                     .map { it.getNeighbours() }
                     .flatten()
-                    .map { it as Point2D }
+                    .map { it as HexPoint2D }
                     .groupingBy { it }
                     .eachCount()
                     .filter { (it.key in curTiles && it.value == 1) || it.value == 2 }
